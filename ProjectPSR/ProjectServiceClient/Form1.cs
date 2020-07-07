a547bf58831873c997b7a6811f27eb85ab9192bb
mitzzz96 <mitzzz96@gmail.com>
2020-07-07 18:41:21 +0200

    dodane przyciski, zabezpieczonie przed odpaleniem kiedy nie ma wgranego, któregoś z pliku

--- ProjectPSR/ProjectServiceClient/Form1.cs
+++ ProjectPSR/ProjectServiceClient/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows;
 
 namespace ProjectServiceClient
 {
@@ -15,6 +18,32 @@ namespace ProjectServiceClient
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                txtEditor1.Text = File.ReadAllText(openFileDialog.FileName);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                txtEditor2.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if((txtEditor1.Text.Length > 0)&&(txtEditor2.Text.Length > 0)){
+               label1.Text = "Tutaj wyskoczy zgodność pliku";
+            }
+            else {
+                MessageBox.Show("Proszę wybrać oba pliki w celu porównania", "Błąd!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
         }
     }
 }
